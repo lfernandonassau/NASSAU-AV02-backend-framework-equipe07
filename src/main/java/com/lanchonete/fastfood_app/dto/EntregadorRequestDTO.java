6c384a6264f8c9f2d82e12f081a67ab9b20bcfcd
Paulo Roberto da Silva <paulinhotoritama@gmail.com>
2025-11-10 15:23:15 -0300

    DTO Entregador

--- src/main/java/com/lanchonete/fastfood_app/dto/EntregadorRequestDTO.java
+++ src/main/java/com/lanchonete/fastfood_app/dto/EntregadorRequestDTO.java
@@ -1,4 +1,12 @@
 package com.lanchonete.fastfood_app.dto;
 
+import lombok.Data;
+
+@Data
 public class EntregadorRequestDTO {
+    private String nome;
+    private String telefone;
+    private String veiculo;
+    private String placa;
+    private Boolean disponibilidade;
 }
--- src/main/java/com/lanchonete/fastfood_app/dto/EntregadorResponseDTO.java
+++ src/main/java/com/lanchonete/fastfood_app/dto/EntregadorResponseDTO.java
@@ -1,4 +1,11 @@
 package com.lanchonete.fastfood_app.dto;
 
+import lombok.Data;
+
+@Data
 public class EntregadorResponseDTO {
+    private Long id;
+    private String nome;
+    private String veiculo;
+    private Boolean disponibilidade;
 }
