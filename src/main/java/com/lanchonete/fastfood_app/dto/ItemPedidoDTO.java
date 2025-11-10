2b5b2413636c9474ad654a3c3674d0f13c87fe4f
Paulo Roberto da Silva <paulinhotoritama@gmail.com>
2025-11-10 15:25:56 -0300

    DTO Itens/Produtos

--- src/main/java/com/lanchonete/fastfood_app/dto/ItemPedidoDTO.java
+++ src/main/java/com/lanchonete/fastfood_app/dto/ItemPedidoDTO.java
@@ -1,4 +1,11 @@
 package com.lanchonete.fastfood_app.dto;
 
+import lombok.Data;
+
+@Data
 public class ItemPedidoDTO {
+    private Long produtoId;
+    private String produtoNome;
+    private Integer quantidade;
+    private Double precoUnitario;
 }
--- src/main/java/com/lanchonete/fastfood_app/dto/ProdutoDTO.java
+++ src/main/java/com/lanchonete/fastfood_app/dto/ProdutoDTO.java
@@ -1,4 +1,12 @@
 package com.lanchonete.fastfood_app.dto;
 
+import lombok.Data;
+
+@Data
 public class ProdutoDTO {
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+    private String imagemUrl;
 }
