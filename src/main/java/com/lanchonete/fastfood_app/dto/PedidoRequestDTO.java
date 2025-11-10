135608d7345be2aa4cb89cab2ffa7f7a650ba803
Paulo Roberto da Silva <paulinhotoritama@gmail.com>
2025-11-10 15:36:45 -0300

    tinhas esquecido de adicionar os Pedidos

--- src/main/java/com/lanchonete/fastfood_app/dto/PedidoRequestDTO.java
+++ src/main/java/com/lanchonete/fastfood_app/dto/PedidoRequestDTO.java
@@ -1,4 +1,10 @@
 package com.lanchonete.fastfood_app.dto;
 
+import lombok.Data;
+import java.util.List;
+
+@Data
 public class PedidoRequestDTO {
+    private Long usuarioId;
+    private List<ItemPedidoRequestDTO> itens;
 }
--- src/main/java/com/lanchonete/fastfood_app/dto/PedidoResponseDTO.java
+++ src/main/java/com/lanchonete/fastfood_app/dto/PedidoResponseDTO.java
@@ -1,4 +1,18 @@
 package com.lanchonete.fastfood_app.dto;
 
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
 public class PedidoResponseDTO {
+    private Long id;
+    private LocalDateTime data;
+    private Double valorTotal;
+    private String status;
+    private Double taxaEntrega;
+    private UsuarioResponseDTO cliente;
+    private EntregadorResponseDTO entregador;
+    private List<ItemPedidoDTO> itens;
+    private NotaFiscalPublicDTO notaFiscal;
 }
