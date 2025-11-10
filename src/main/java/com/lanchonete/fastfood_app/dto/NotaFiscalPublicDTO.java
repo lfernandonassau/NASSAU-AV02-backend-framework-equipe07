4ea79b19404953d487cb69f8ed96e11250ed1e13
Paulo Roberto da Silva <paulinhotoritama@gmail.com>
2025-11-10 15:32:33 -0300

    DTO Nota Fiscal e Pedidos

--- src/main/java/com/lanchonete/fastfood_app/dto/NotaFiscalPublicDTO.java
+++ src/main/java/com/lanchonete/fastfood_app/dto/NotaFiscalPublicDTO.java
@@ -1,4 +1,11 @@
 package com.lanchonete.fastfood_app.dto;
 
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
 public class NotaFiscalPublicDTO {
+    private Long numero;
+    private LocalDateTime dataEmissao;
+    private Double valorTotal;
 }
